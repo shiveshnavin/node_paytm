dca66f099b6cf096098c81ba9d6772098350c2bc
shiveshnavin <shiveshnavin@gmail.com>
2019-03-26 15:58:07 +0530

    Added new callbacks

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports=(app,express,callbacks)=>{
 
-    return require('./app/routes/payment_route.js')(app,express,callbacks) 
+    var module = {};
 
+    module.Transaction=require('./app/models/np_transaction.model.js')
+    module.User=require('./app/models/np_user.model.js')
+
+    require('./app/routes/payment_route.js')(app,express,callbacks) 
+
+    return module;
 };
  
