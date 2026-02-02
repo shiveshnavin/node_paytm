81b62c67c50cf41cf7ae5a60bfed50641457a0f9
shiveshnavin <shiveshnavin@gmail.com>
2026-02-02 19:20:59 +0530

    PayU in progress:

--- example.js
+++ example.js
@@ -7,25 +7,28 @@ var MONGOURL="mongodb+srv://username:pasws@host.net/dbname";
  * Uncomment in case you want to use multidborm 
  * https://www.npmjs.com/package/multi-db-orm
 
- const { MultiDbORM, FireStoreDB, MongoDB, SQLiteDB, Sync } = require("multi-db-orm");
-var mongodb = new MongoDB(MONGOURL);
-app.multidborm = mongodb;
 
 */
+const { MultiDbORM, FireStoreDB, MongoDB, SQLiteDB, Sync } = require("multi-db-orm");
+var mongodb = new SQLiteDB();
+app.multidborm = mongodb;
 
 app.set('np_config', {
     "host_url":"http://127.0.0.1:5542", 
     "view_path":"/../views/",
-    "paytm_url":"https://securegw-stage.paytm.in",
-    "MID":"XXXXX",
+    "payu_url":"https://test.payu.in",
+    //"razor_url":"https://api.razorpay.com/v1/",
+    //"open_money_url":"https://sandbox.openmoney
+    "MID":"4fd0a71ccddacdb3023063340ede80a5adce7cb305e2ca99f772aa4f846f4f12",
     "WEBSITE":"WEBSTAGING",
-    "KEY":"XXXXX",
+    "KEY":"4nuyEF",
+    "SECRET":"dGZLFqrZOR9ZWsUxRV0E10EE6hE36Xda",
     "CHANNEL_ID":"WAP", 
     "INDUSTRY_TYPE_ID":"Retail",
     "homepage":"/",
     "path_prefix":"_pay",
     "theme_color":"#231530",
-    "db_url":MONGOURL // Remove this property in case you want to use multidborm
+    // "db_url":MONGOURL // Remove this property in case you want to use multidborm
 });
 
 if(process.env.CONFIG){
